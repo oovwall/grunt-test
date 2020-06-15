66cf468a5678d5f746c23afe6bd7445da0b51963
oovwall <oovwall@163.com>
2020-06-16 02:49:28 +0800

    init

--- gruntfile.js
+++ gruntfile.js
@@ -1,51 +1,135 @@
+const sass = require('sass')
+const loadGruntTasks = require('load-grunt-tasks')
+
 module.exports = grunt => {
+  const data = {
+    menus: [
+      {
+        name: 'Home',
+        icon: 'aperture',
+        link: 'index.html'
+      },
+      {
+        name: 'Features',
+        link: 'features.html'
+      },
+      {
+        name: 'About',
+        link: 'about.html'
+      },
+      {
+        name: 'Contact',
+        link: '#',
+        children: [
+          {
+            name: 'Twitter',
+            link: 'https://twitter.com/w_zce'
+          },
+          {
+            name: 'About',
+            link: 'https://weibo.com/zceme'
+          },
+          {
+            name: 'divider'
+          },
+          {
+            name: 'About',
+            link: 'https://github.com/zce'
+          }
+        ]
+      }
+    ],
+    pkg: require('./package.json'),
+    date: new Date()
+  }
+
   grunt.initConfig({
-    version: '1.0.0',
-    build: {
-      css: '> build css action',
-      js: '> build js action'
+    cleanempty: {
+      options: {},
+      src: ['dist/*'],
     },
-    clean: {
-      temp: 'temp/**'
+    sass: {
+      options: {
+        sourceMap: true,
+        implementation: sass
+      },
+      main: {
+        files: {
+          'dist/assets/styles/main.css': 'src/assets/styles/main.scss'
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'dist/assets/styles/main.min.css': ['dist/assets/styles/main.css']
+        }
+      }
+    },
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['@babel/preset-env']
+      },
+      main: {
+        files: {
+          'dist/assets/scripts/main.js': 'src/assets/scripts/main.js'
+        }
+      }
+    },
+    swigtemplates: {
+      options: {
+        defaultContext: data,
+        templatesDir: 'src/'
+      },
+      production: {
+        dest: 'dist/',
+        src: ['src/*.html']
+      }
+    },
+    useref: {
+      copy: {
+        test: {
+          files: [
+            {expand: true, cwd: '/', src : ['.'], dest: 'dist/' }
+          ]
+        }
+      },
+      // specify which files contain the build blocks
+      html: 'dist/*.html',
+      // explicitly specify the temp directory you are working in
+      // this is the the base of your links ( "/" )
+      temp: 'dist'
+    },
+    watch: {
+      css: {
+        files: ['src/assets/styles/*.scss'],
+        tasks: ['sass']
+      },
+      js: {
+        files: ['src/assets/scripts/*.js'],
+        tasks: ['babel']
+      },
+      html: {
+        files: ['src/*.html'],
+        tasks: ['swigtemplates:production']
+      }
     }
   })
 
-  grunt.registerTask('start', () => {
-    console.log('grunt start', grunt.config('version')) // 读取配置
-  })
+  loadGruntTasks(grunt)
+  grunt.registerTask('default', [
+    'cleanempty',
+    'sass',
+    'cssmin',
+    'babel',
+    'swigtemplates:production',
+    'useref',
+    'watch'
+  ])
 
-  grunt.registerTask('end', () => {
-    console.log('grunt end')
-  })
-
-  // 标记同步任务失败，如果忽略错误执行：yarn grunt --force
-  grunt.registerTask('bad', () => {
-    console.log('grunt failed')
-    return false
-  })
-
-  grunt.registerTask('async', function () {
-    const done = this.async()
-    setTimeout(() => {
-      console.log('this is a async task')
-      done()
-    }, 3000)
-  })
-
-  // 标记异步任务失败
-  grunt.registerTask('async-bad', function () {
-    const done = this.async()
-    setTimeout(() => {
-      console.log('this is a async task')
-      done(false)
-    }, 3000)
-  })
-
-  grunt.registerTask('default', ['start', 'bad', 'end'])
-  grunt.registerMultiTask('build', function () {
-    console.log(`build ${this.target}, do ${this.data}`)
-  })
-
-  // 使用插件构建任务
-  grunt.loadNpmTasks('grunt-contrib-clean')
 }
